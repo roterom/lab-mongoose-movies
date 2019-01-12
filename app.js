ca73b3a5f8ea181640d626bce35b57d021a65752
Julio García Pérez <juliojose.garcia@11paths.com>
2019-01-12 14:13:18 +0100

    refactor project structure

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+require('./config/db.config');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, () => console.info(`App listen at ${PORT} port`));
